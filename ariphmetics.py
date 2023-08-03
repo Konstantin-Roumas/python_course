f280fabb28e3bffa8b5e76cb473672351d0dd77a
Andre <andrey98714@gmail.com>
2023-08-03 18:26:00 +0300

    refactored directory

--- ariphmetics.py
+++
@@ -1,9 +0,0 @@
-minutes = int(input())
-input_hours = int(input())
-input_minutes = int(input())
-total_minutes = minutes + (input_hours // 60) + input_minutes
-result_hours = total_minutes // 60
-result_minutes = total_minutes % 60
-print(result_hours)
-print(result_minutes)
-print('Hello World!')

bab4815e567d387d04dc3568c2c7b9bf74a73d21
Andre <andrey98714@gmail.com>
2023-08-07 21:22:11 +0300

    lessons 29-34

---
+++ andrey/Hexlet/Result_of_logical_operations.py
@@ -0,0 +1,5 @@
+def string_or_not(string):
+    print(isinstance(string, str) and 'yes' or 'no')
+
+
+string_or_not(124)
---
+++ andrey/Hexlet/While_loop.py
@@ -0,0 +1,8 @@
+def print_numbers(n):
+    while n >= 1:
+        print(n)
+        n -= 1
+    print('finished!')
+
+
+print_numbers(50)
